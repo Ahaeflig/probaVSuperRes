f29e3d685a2d557bac969aacaac70f7092407145
Adan Häfliger <adan.haefliger@gmail.com>
2019-07-10 22:03:38 +0200

    Add simple CNN, + clearMSE

--- ProbaData.py
+++ ProbaData.py
@@ -10,8 +10,6 @@ import tensorflow as tf
 
 from supreshelper import *
 
-from itertools import cycle
-
 from typing import List
 
 PATCH_SIZE = 32
@@ -23,7 +21,6 @@ class Scene() :
     """Data holder for scene images paths containing list of paths and paths to all
     relevant images in the scene
     """
-    
     def __init__(self, qms: List[str], lrs: List[str], sm: str, uplr:str, hr: str=None):
         self.qms = qms
         self.lrs = lrs
@@ -45,14 +42,10 @@ class ProbaVDataset():
         self.train_samples = glob(data_dir +  "train/*/*") 
         self.test_samples = glob(data_dir + "test/*/*")
         
-        self.train_cycle = cycle(self.train_samples)
-        self.test_cycle = cycle(self.test_samples)
-        
         self._check_dataset()
         
         # We store current pivot to vizualize, a bit hacky for now
         self.pivot = None
-        
         
     
     def _check_dataset(self):
@@ -64,11 +57,14 @@ class ProbaVDataset():
         for scene_path in self.train_samples + self.test_samples:
             scene = self.get_scene_content(scene_path)
             assert (len(scene.qms) >= 9), "Less than 9 images at scene" + scene_path
-            assert (len(scene.lrs) == len(scene.qms)), "Number of QMS and LRS images are not identical at scene " + scene_path + ": " + str(len(scene.lrs) + 1) + " / " + str(len(scene.qms))
             assert (os.path.isfile(scene.sm)), "Missing SM image at scene " + scene_path
+            assert (len(scene.lrs) == len(scene.qms) ), "Number of QMS and LRS  images are not identical at scene " + scene_path + ": " + str(len(scene.lrs)) + " / " + str(len(scene.qms))
+            
             if scene_path.split("/")[1] == "train":
                 assert (os.path.isfile(scene.hr)), "Missing HR image at scene " + scene_path
-        
+            else:
+                assert (os.path.isfile(scene.uplr)), "Missing up_LR image at scene " + scene_path
+                
     def get_scene_content(self, scene_path: str):
         """Finds the file for a given scene (folder) in the dataset
 
@@ -82,10 +78,14 @@ class ProbaVDataset():
         lrs = glob(scene_path + "/LR*.png")
         sm = scene_path + "/SM.png"
         hr = scene_path + "/HR.png"
-        up_lr = scene_path + "/up_LR.png"
+        up_lr = scene_path + "/up_LR.npy"
         
         return Scene(qms, lrs, sm, up_lr, hr)
     
+    
+    """
+    Patches Block
+    """
     def load_image(self, img_path, is_quality_map=False):
         image = tf.image.decode_png(tf.io.read_file(img_path), dtype=tf.uint16)
         input_image = tf.cast(image, tf.float32)
@@ -94,9 +94,6 @@ class ProbaVDataset():
         else:
             return input_image
         
-    """
-    Patches Block
-    """
     # Jitter is basically 40 - 32
     def load_qms_lrs_patches(self, scene: Scene, pivot: tf.Tensor, jitter: int=8):
         j_d = (int)((PATCH_SIZE + 8) / 2)
@@ -176,38 +173,64 @@ class ProbaVDataset():
     """
     Singe Image Block
     """
+    def load_data(self, path: str):
+        """ Load data stored in .npy format
+            
+            Args:
+                path: Path to the .npy file
+                
+            Return:
+                loaded numpy array as a Tensor
+        """
+        data = np.load(path)
+        return tf.expand_dims(tf.convert_to_tensor(data, dtype=tf.float32), axis=2)
+    
     @tf.function
-    def get_uplr_hr(self, scene, augment=False):
-        lr, hr = self.load_image(scene.uplr), self.load_image(scene.hr)
+    def load_train_image(self, lr_hr_sm, augment=True):
+        lr_hr_sm = tf.expand_dims(lr_hr_sm, axis=2)
+        
+        w = tf.shape(lr_hr_sm)[1]
+        w = w // 3
+        lr = lr_hr_sm[:, :w, :]
+        hr = lr_hr_sm[:, w:2*w, :]
+        sm = lr_hr_sm[:, 2*w:3*w, :]
+        lr = tf.cast(lr, tf.float32)
+        hr = tf.cast(hr, tf.float32)
+        sm = tf.cast(sm, tf.float32)
         
         if augment:
-            return self.random_jitter(lr, hr)
+            return self.random_jitter(lr, hr, sm)
         else:
-            return lr, hr
+            return lr, self.apply_mask(hr, sm)
+    
+    def apply_mask(self, hr, sm):
+        return  tf.where(sm > 0, hr, np.NaN)
     
     def resize(self, image, height, width):
         return tf.image.resize(image, [height, width], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
     
-    def random_crop(self, lr, hr):
-        stacked_image = tf.stack([lr, hr], axis=0)
-        cropped_image = tf.image.random_crop(stacked_image, size=[2, 384, 384, 1])
-        return cropped_image[0], cropped_image[1] 
+    def random_crop(self, lr, hr, sm):
+        stacked_image = tf.stack([lr, hr, sm], axis=0)
+        cropped_image = tf.image.random_crop(stacked_image, size=[3, 384, 384, 1])
+        return cropped_image[0], cropped_image[1], cropped_image[2]
         
     @tf.function()
-    def random_jitter(self, lr, hr):
-        lr, hr = self.resize(lr, 429, 429), self.resize(hr, 429, 429)
-        lr, hr = self.random_crop(lr, hr)
+    def random_jitter(self, lr, hr, sm):
+        lr, hr, sm = self.resize(lr, 429, 429), self.resize(hr, 429, 429), self.resize(sm, 429, 429)
+        lr, hr, sm = self.random_crop(lr, hr, sm)
 
         if tf.random.uniform(()) >= 0.5:
             lr = tf.image.flip_left_right(lr)
             hr = tf.image.flip_left_right(hr)
+            sm = tf.image.flip_left_right(sm)
             
         if tf.random.uniform(()) >= 0.5:
             lr = tf.image.flip_up_down(lr)
             hr = tf.image.flip_up_down(hr)
-
-        return lr, hr
+            sm = tf.image.flip_up_down(sm)
+        
+        return lr, self.apply_mask(hr, sm)
     
-    def lr_hr_gen(self):
-        yield next(self.train_cycle), next(self.test_cycle), 
+    def load_test_image(self, filepath):
+        return self.load_data(filepath)
         
